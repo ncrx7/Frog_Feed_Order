13b6f874964cadc0a8991893550bfc07fe4ec16f
ncrx7 <buysal31@gmail.com>
2024-08-19 13:09:01 +0300

    feat: Added grid object class

---
+++ Assets/Scripts/Grid/GridObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObject<T>
+{
+     //I made this class generic so that it would be more dynamic and convenient for us to put another object.
+     //The object class that exists on any grid object.
+    GridSystem<GridObject<T>> grid;
+    private int _x;
+    private int _y;
+    private T _gridObject;
+
+    public GridObject(GridSystem<GridObject<T>> grid, int x, int y)
+    {
+        this.grid = grid;
+        this._x = x;
+        this._y = y;
+    }
+
+    public void SetValue(T gridObject)
+    {
+        this._gridObject = gridObject;
+    }
+
+    public T GetValue()
+    {
+        return _gridObject;
+    }
+}
